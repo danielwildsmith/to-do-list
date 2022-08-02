bf59f69a36243150498ad76f8e9f9d69faa4bf33
Daniel <danielwildsmith@ufl.edu>
2022-08-02 18:37:45 -0400

    Create addTask function

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const taskInput = document.querySelector('#task-input');
 const taskButton = document.querySelector('#task-add-button');
-const taskList = document.querySelector('task-list');
+const taskList = document.querySelector('.task-list');
 
 taskButton.addEventListener('click', addTask);
 
 
 function addTask(event) {
     event.preventDefault();
-    console.log('hello');
+    
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
+
+    const completeButton = document.createElement('button');
+    completeButton.classList.add('complete-task-btn');
+    completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    taskDiv.appendChild(completeButton);
+
+    const taskName = document.createElement('li');
+    taskName.classList.add('task-name');
+    taskName.innerText = taskInput.value;
+    taskDiv.appendChild(taskName);
+
+    const editButton = document.createElement('button');
+    editButton.classList.add('edit-task-btn');
+    editButton.innerHTML = '<i class="fa-solid fa-wrench"></i>';
+    taskDiv.appendChild(editButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-task-btn');
+    deleteButton.innerHTML = '<i class="fa-solid fa-x"></i>';
+    taskDiv.appendChild(deleteButton);
+
+    taskList.appendChild(taskDiv);
 }
